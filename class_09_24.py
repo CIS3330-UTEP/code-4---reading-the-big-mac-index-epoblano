d2814d75ad3dfd29c0af350ac5eea68794f0aaec
epoblano <epoblano@miners.utep.edu>
2024-09-26 09:48:26 -0600

    Create class_09_24.py

---
+++ class_09_24.py
@@ -0,0 +1,23 @@
+from winreg import QueryValue
+import pandas as pd
+
+df = pd.read_csv('./big-mac-full-index.csv')
+
+print(df)
+
+country_code = "RUS"
+
+query f"(iso_a3 == '{country_code}')"
+
+print(len(df))
+##sub_df = df.query(query_text)
+#print(len(sub_df))
+
+#print(sub_df)
+#print(sub_df.iloc[21])
+#print("\n")
+#print(sub_df.iloc[21])
+
+#print(subdf['dollar_price'].mean())
+
+#new_query
